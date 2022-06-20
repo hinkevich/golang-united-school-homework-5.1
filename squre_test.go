febb31817428d20c9911f31408123b9c85ea983e
hinkevich <hinkevich@gmail.com>
2022-06-20 19:31:42 +0300

    add changes

--- squre_test.go
+++ squre_test.go
@@ -10,10 +10,10 @@ type testDataEndPoint struct {
 func TestSquare(t *testing.T) {
 	tests := []testDataEndPoint{
 		testDataEndPoint{sq: Square{Point{x: 0, y: 0}, 3}, want: Point{3, -3}},
-		testDataEndPoint{sq: Square{Point{x: -5, y: -6}, 3}, want: Point{-2, -3}},
-		testDataEndPoint{sq: Square{Point{x: 0, y: 0}, 3}, want: Point{3, -3}},
-		testDataEndPoint{sq: Square{Point{x: 0, y: 0}, 3}, want: Point{3, -3}},
-		testDataEndPoint{sq: Square{Point{x: 0, y: 0}, 3}, want: Point{3, -3}},
+		testDataEndPoint{sq: Square{Point{x: -5, y: -6}, 3}, want: Point{-2, -9}},
+		testDataEndPoint{sq: Square{Point{x: 5, y: 5}, 5}, want: Point{10, 0}},
+		testDataEndPoint{sq: Square{Point{x: 10, y: -10}, 3}, want: Point{13, -13}},
+		testDataEndPoint{sq: Square{Point{x: -10, y: 10}, 3}, want: Point{-7, 7}},
 	}
 	for i, test := range tests {
 		got := test.sq.End()
